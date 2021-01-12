32ef5f33360eb50dc26d4c15c5007902c6904d63
Antoine LOUBIERES <anloubie@e1r8p14.42.fr>
2021-01-12 15:57:34 +0100

    fixed reverse proxy phpmyadmin

--- srcs/PhpMyAdmin/srcs/config.inc.php
+++ srcs/PhpMyAdmin/srcs/config.inc.php
@@ -32,7 +32,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
-$cfg['PmaAbsoluteUri'] = '/';
+$cfg['PmaAbsoluteUri'] = '/phpmyadmin';
 
 /**
  * phpMyAdmin configuration storage settings.
